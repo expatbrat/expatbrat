cf3e90dc2194973cd3ca4973a8b560bb0b0694e4
Alexander Ronalds <alexanderronalds@alexanders-mbp.lan>
2020-05-18 10:28:36 -0700

    v01, needs styling and less crappy code

--- app.js
+++ app.js
@@ -1,53 +1,58 @@
 
 
 $(() => {
-    const randomEthan = ['img/ethan_laugh1.jpg', 'img/ethan_slap.gif', 'img/ethan_angry.jpg']
-    // const showButton = () => {
-    //     $('#button').show()
-    // }
-
-    // showButton()
-
-    $('form').on('submit', (event) => {
-        event.preventDefault();
-        $.ajax(
-            {
-                url:'https://icanhazdadjoke.com/slack',
-            }
-        ).then(
-            (data) => {
-                console.log(data.attachments[0].fallback);
-                $('h3').html(data.attachments[0].fallback);
-                $('#dad-bubble-image').css('display', 'block');
-                $('.ethan-bubble').children().hide();
-                // $('.book').css('background-image', `url('img/ethan_slap.gif')`)
-                createBackgroundListner();
-                console.log(createBackgroundListner);
-
-            },
-            (error) => {
-                console.log(error);
-            }
-        )
-    })
-
-
+    const randomEthan = ['img/ethan_laugh1.jpg', 'img/ethan_smile.jpg', 'img/ethan_unimpressed.jpg', 'img/laughingEthan.jpg', 'img/ethan-excited.jpg']
+    const randomEthanQuote = [`Don't quit your day job.`, 'That was so cheesy!', 'Your jokes suck, loser.', 'Bahahaha! Good one dad!', 'Tell me another one!']
+    const bubblePosition = ['translateY(40%) translateX(190%)', 'translateY(80%) translateX(190%)', 'translateY(80%) translateX(190%)', 'translateY(60%) translateX(190%)', 'translateY(60%) translateX(190%)'];
     const createBackgroundListner = () => {
       $('body').on('click', (event) => {
+        const randomEthanIndex = Math.floor(Math.random() * randomEthan.length);
+        console.log(randomEthanIndex);
         $('.dad-bubble').children().hide();
-        $('.book').css('background-image', `url(${randomEthan[0]})`);
+        $('#button').css('display', 'none');
+        $('.book').css('background-image', `url(${randomEthan[randomEthanIndex]})`);
         console.log($('#dad-bubble-image'))
-        $('#ethan-bubble-image').attr('src', 'img/think_bubble.png');
+        $('#ethan-h1-text').css('transform', bubblePosition[randomEthanIndex])
         $('.ethan-bubble').children().show();
-        $('#ethan-h1-text').html(`You're so funny dad!`);
-        $('body').removeEventListener()
+        $('#ethan-bubble-image').attr('src', 'img/think_bubble.png');
+        $('#ethan-h1-text').html(`${randomEthanQuote[randomEthanIndex]}`);
+        $('body').on('click', (event) => {
+          resetPage()
+        })
+      })
+    }
+
+
+    const resetPage = () => {
+      console.log('yourmom')
+      $('#ethan-bubble-image').attr('src', 'img/speech_bubble_4.png');
+      $('#ethan-h1-text').html(`Tell me another joke Dad!`);
+      $('.book').css('background-image', `url(img/alex_ethan.jpg)`);
+      $('#button').css('display', 'block');
+      $('#ethan-h1-text').css('transform', 'translateY(20%) translateX(190%)');
+    }
+
+  $('form').on('submit', (event) => {
+      event.preventDefault();
+      $.ajax(
+          {
+              url:'https://icanhazdadjoke.com/slack',
+          }
+      ).then(
+          (data) => {
+            console.log(data);
+             const refreshButton = () => {
+               $('.dad-bubble').children().show();
+               $('.ethan-bubble').children().hide();
+               $('h3').html(data.attachments[0].fallback);
+               createBackgroundListner();
+             }
+             refreshButton();
+          },
+          (error) => {
+            console.log(error);
+      })
     })
-  }
-
-
-
-
-
 })
 
 
